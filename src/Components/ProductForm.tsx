461322c03fef98b8ea8f7556f3074d33331dacca
Guilherme Possebon <gpossebon67@gmail.com>
2023-05-04 16:03:20 -0300

    Adição do botão de upload de imagem

--- src/Components/ProductForm.tsx
+++ src/Components/ProductForm.tsx
@@ -10,13 +10,15 @@ interface ExistingType {
   existingPrice?: object | string
   price?: string
   _id?: string
+  images?: File
 }
 
 export default function ProductForm({
   _id,
   title: existingTitle,
   description: existingDescription,
-  price: existingPrice
+  price: existingPrice,
+  images
 }: ExistingType) {
   const [title, setTitle] = useState(existingTitle)
   const [description, setDescription] = useState(existingDescription)
@@ -56,6 +58,28 @@ export default function ProductForm({
           onChange={(e) => setTitle(e.target.value)}
         />
       </label>
+      <label htmlFor="Photo">Photos</label>
+      <div className="mb-2">
+        <label className="w-24 h-24 border text-center flex flex-col items-center justify-center text-gray-500 gap-1 text-sm rounded-lg bg-gray-300 cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          Upload
+          <input type="file" name="image-file" id="image-file" className='hidden' />
+        </label>
+        {!images && <div>No photos in this produtcs</div>}
+      </div>
       <label htmlFor="description">
         Description
         <textarea
