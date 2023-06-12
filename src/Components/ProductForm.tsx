a434ec8c25ac82f5d82eada0068cb7aee7a4eb66
Guilherme Possebon <gpossebon67@gmail.com>
2023-06-11 21:38:35 -0300

    Remoção de console.log inuteis

--- src/Components/ProductForm.tsx
+++ src/Components/ProductForm.tsx
@@ -85,14 +85,6 @@ export default function ProductForm({
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100
           setProgress(progress)
-          switch (snapshot.state) {
-            case 'paused':
-              console.log('Upload is paused')
-              break
-            case 'running':
-              console.log('Upload is running')
-              break
-          }
         },
         (error) => {
           switch (error.code) {
@@ -106,7 +98,6 @@ export default function ProductForm({
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            console.log('File available at', downloadURL)
             setProductUrl(downloadURL)
           })
         }
