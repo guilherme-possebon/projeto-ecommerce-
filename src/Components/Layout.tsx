5b2f6f0d77c053579cf97f6248173681c07c907e
Guilherme Possebon <47215500+guilherme-possebon@users.noreply.github.com>
2023-08-18 23:03:36 -0300

    Adição de duas features
    adição do DarkMode através do esquema de preferencia de cores do sistema do usuário, e adição de um menu de hambúrguer para melhor responsividade no mobile

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import { useSession, signIn, signOut } from 'next-auth/react'
 import Nav from '@/Components/Nav'
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 
 type LayoutProp = {
   children: ReactNode
 }
-
 export default function Layout({ children }: LayoutProp) {
+  const [showNav, setShowNav] = useState(false)
   const { data: session } = useSession()
   if (!session) {
     return (
@@ -32,10 +32,28 @@ export default function Layout({ children }: LayoutProp) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <div className="min-h-screen flex bg-bgGray dark:bgDarkMode">
-        <Nav />
-        <div className="bg-white dark:bgDarkMode flex-grow mr-2 mt-2 mb-2 rounded-lg p-4">
-          {children}
+      <div className="bg-bgGray">
+        <button onClick={() => setShowNav(!showNav)} className="md:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
+        <div className=" flex min-h-screen dark:bgDarkMode">
+          <Nav show={showNav} />
+          <div className="bg-white dark:bgDarkMode flex-grow mr-2 mt-2 mb-2 rounded-lg p-4">
+            {children}
+          </div>
         </div>
       </div>
     </>
