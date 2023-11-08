d7a63145d36d9a477c1313c16e355e6da2ec7d24
Guilherme Possebon <47215500+guilherme-possebon@users.noreply.github.com>
2023-11-08 15:58:21 -0300

    tentando arrumar o mesmo bug do auth

--- src/Context/ProductContext.tsx
+++ src/Context/ProductContext.tsx
@@ -1,4 +1,9 @@
-import React, { createContext, useContext, useState, type ReactNode } from 'react'
+import React, {
+  createContext,
+  useContext,
+  useState,
+  type ReactNode
+} from 'react'
 
 interface ProductContextType {
   productSaved?: boolean
