c1f43d6e785a6b8ad70e66a75b6594392731b3b8
Guilherme Possebon <47215500+guilherme-possebon@users.noreply.github.com>
2023-11-26 20:04:16 -0300

    Correção na escrita da pagina delete

--- src/pages/products/delete/[...id].tsx
+++ src/pages/products/delete/[...id].tsx
@@ -37,19 +37,18 @@ export default function DeleteProductPage() {
       setProductDeleted(true)
       goBack()
     } catch (error) {
-      // Handle the error here
+      console.error(error)
     }
   }
 
-  // TODO testar para ver se esta tudo certo aqui "&quot{productInfo?.title}&ldquo?"
   return (
     <Layout>
       <h1 className="text-center">
-        Você realmente quer deletar o produto {productInfo?.title}?
+        Você realmente quer deletar o produto <q>{productInfo?.title}</q>?
       </h1>
 
       <div className="flex gap-2 justify-center">
-        <button onClick={() => deleteProduct} autoFocus className="btn-red">
+        <button onClick={deleteProduct} autoFocus className="btn-red">
           Sim
         </button>
         <button onClick={goBack} className="btn-default">
